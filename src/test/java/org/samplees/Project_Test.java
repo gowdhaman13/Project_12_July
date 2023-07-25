042c6244c80af6b2464db783ad852e4a831303ad
gowdhaman13 <gowdhaman04@gmail.com>
2023-07-25 19:37:46 +0530

    project change

--- src/test/java/org/samplees/Project_Test.java
+++ src/test/java/org/samplees/Project_Test.java
@@ -1,9 +1,13 @@
 package org.samplees;
 
 public class Project_Test {
-public static void main(String[] args) {
+private void hi() {
+System.out.println("java selenium");
+}
+	public static void main(String[] args) {
 	
-	
+	System.out.println("hello");
+	System.out.println("gowdhaman");
 	System.out.println("welcome");
 }
 }
